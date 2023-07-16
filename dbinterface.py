6039f28c2feabfdf128e15e7eeede6e1b39e0660
Innaka <inna@ef7.ru>
2023-07-16 14:38:46 +1000

    commit 15

--- dbinterface.py
+++ dbinterface.py
@@ -16,23 +16,42 @@ class Seen(Base):
 
 
 # добавление записи в бд
-#db_url_object = "postgresql://postgres:postgres@localhost/vkinder"
-engine = create_engine(db_url_object)
-Base.metadata.create_all(engine)
-with Session(engine) as session:
-    to_bd = Seen(profile_id=2, worksheet_id=2)
-    session.add(to_bd)
-    session.commit()
+
+def add_profile(engine, profile_id, worksheet_id):
+    with Session(engine) as session:
+        add_to_bd = Seen(profile_id=profile_id, worksheet_id=worksheet_id)
+        session.add(add_to_bd)
+        session.commit()
+
+#engine = create_engine(db_url_object)
+#Base.metadata.create_all(engine)
 
 # извлечение записей из БД
+def find_profile(engine, profile_id, worksheet_id):
+    with Session(engine) as session:
+        find_in_bd = session.query(Seen).filter(
+            Seen.profile_id == profile_id,
+            Seen.worksheet_id == worksheet_id
+        ).first()
+        return True if find_in_bd else False
 
-engine = create_engine(db_url_object)
-with Session(engine) as session:
-    from_bd = session.query(Seen).filter(Seen.profile_id==2).all()
-    for item in from_bd:
-        print(item.worksheet_id)
+
+#engine = create_engine(db_url_object)
+#with Session(engine) as session:
+#    from_bd = session.query(Seen).filter(Seen.profile_id==2).all()
+#    for item in from_bd:
+#        print(item.worksheet_id)
 
 if __name__ == '__main__':
     #bot = BotFront(comunity_token, acces_token)
     bot.event_handler()
+
+if __name__ == '__main__':
+    profile_id = []
+    worksheet_id = []
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+    add_profile(engine, profile_id, worksheet_id)
+    result = finde_profile(engine, profile_id, worksheet_id)
+    print(result)
             
