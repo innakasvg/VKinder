9147ad0d943263b489c43a58ebee762c279b5b22
Innaka <inna@ef7.ru>
2023-07-16 14:41:04 +1000

    commit 17

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
 comunity_token = ''
 acces_token = ''
 db_url_object = "postgresql+psycopg2://postgres:postgres@localhost/vkinder"
-
-#acces_token = 'vk1.a.zSK-ghw1wuptQ98ZGA111Bg8he9WV1b7m2PZEQJH88dG0BYGIP1vOO-zW5R78z1ptn3Gj_rru2gItKo8_DvdSh2yzGFklCpvJvfJHImYh2mrSdLWQMgrlCrEY-OiydyuycMFoq_U1ZVMqhyeuI5bKRAvG0ACUNQgO7zMQpZwyrBfsucHuK6pf44oBCl4qjF_'
-#"postgresql://postgres:YOURPASSWORD@localhost:5432/vKinder_bot_db"
-#https://oauth.vk.com/blank.html#access_token=vk1.a.zSK-ghw1wuptQ98ZGA111Bg8he9WV1b7m2PZEQJH88dG0BYGIP1vOO-zW5R78z1ptn3Gj_rru2gItKo8_DvdSh2yzGFklCpvJvfJHImYh2mrSdLWQMgrlCrEY-OiydyuycMFoq_U1ZVMqhyeuI5bKRAvG0ACUNQgO7zMQpZwyrBfsucHuK6pf44oBCl4qjF_&expires_in=86400&user_id=7887184
-#https://oauth.vk.com/authorize?client_id=51692504&display=page&redirect_uri=http://example.com/callback&scope=friends&response_type=token&v=5.131&state=123456
-#https://oauth.vk.com/blank.html#access_token=vk1.a.dr7lhONtdLbClxTrR0eK_Vajy_yYpqdXz1fObEG2a169h_Be2pM0x7InBtisR1CUocXND8-FnU2NbN1cNXHc0RjKUlzfm0C_VpzhbHNKb_9IGzhwT6Righ3-GekBCjR0eEwAq2ECEIu2lcTVv7B9dL6RIdBkvXfhlZhGPpcRBFBgep7W_hpbPfLI7J2oNttL&expires_in=86400&user_id=7887184&state=123456
